244528be82946eb05fc995201b7ae178c68ee5db
Jose Manriquez <josejlm2@neo.tamu.edu>
2014-10-05 01:17:01 -0500

    2nd initial commit ??

---
+++ test.php
@@ -0,0 +1,15 @@
+
+
+<?php 
+require 'autoload.php';
+
+$app_id = 'biMGnMGZKMRqeZFLXZDFQP84ln8gKqPzj0oV71TM';
+$rest_key = 'nqlhAuDv6A7ssy1eJCBT8kwAXbY4rBlsiu22ONcL';
+$master_key = 'u53tdzbiGrwUHbD5ciBoPBxrDFiD3SL0GQo930yC';
+
+
+ParseClient::initialize( $app_id, $rest_key, $master_key );
+
+?>
+
+hello
